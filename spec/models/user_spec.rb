0b75ac5b52e702cabca72f818b3a4e53278f5dad
Anna Smolentzov <annasmolentzov@gmail.com>
2019-04-14 09:55:35 -0600

    Adds relationship between user and user_items

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,8 @@ describe User do
   describe 'Validations' do
     it { should validate_presence_of(:name) }
   end
+  
+  describe 'Relationships' do
+    it { should have_many(:user_items) }
+  end
 end
