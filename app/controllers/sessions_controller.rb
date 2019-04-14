25d5627c5c719842cd048b1053e2c6f476de00b9
Anna Smolentzov <annasmolentzov@gmail.com>
2019-04-14 15:48:20 -0600

    Adds user to session and updates current_user method to look to it

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,11 @@
 class SessionsController < ApplicationController
   def create
-    
-    redirect_to profile_path
+    user = User.find_by(name: params[:user_name])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to profile_path
+    else 
+      redirect_to root_path
+    end
   end
 end
