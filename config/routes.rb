882cdee7c5b6c899abeb7be3c53e84470346e199
Anna Smolentzov <annasmolentzov@gmail.com>
2019-04-14 15:39:22 -0600

    Adds route and controller for root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+  root 'welcome#index'
   get '/profile', to: 'users#show'
   post '/profile', to: 'user_items#create'
 end
