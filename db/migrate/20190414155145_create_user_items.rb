14b91b3b54be866495144017a0b05897814d9d19
Anna Smolentzov <annasmolentzov@gmail.com>
2019-04-14 09:53:44 -0600

    Adds database table for user_items

---
+++ db/migrate/20190414155145_create_user_items.rb
@@ -0,0 +1,8 @@
+class CreateUserItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :user_items do |t|
+      t.references :user, foreign_key: true
+      t.references :item, foreign_key: true
+    end
+  end
+end
