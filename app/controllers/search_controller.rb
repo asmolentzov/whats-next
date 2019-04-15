11d97b8335472c5d03b6af9150005388b1cd3508
Anna Smolentzov <annasmolentzov@gmail.com>
2019-04-15 15:51:05 -0600

    Refactors search to use a facade

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,5 @@
 class SearchController < ApplicationController
   def show
-    recs = RecommendationService.new.get_recommendations(Item.create(name: "Harry Potter", item_type: 'book'))
-    @recommendations = recs[:Similar][:Results].map do |rec|
-      Item.find_or_create_by(name: rec[:Name], item_type: rec[:Type])
-    end
+    @facade = SearchFacade.new
   end
 end
