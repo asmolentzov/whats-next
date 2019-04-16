d44135d2af03c5f3c6f0da38a7661fa1141030bb
Anna Smolentzov <annasmolentzov@gmail.com>
2019-04-16 16:06:36 -0600

    Allows for POST request from JS

--- app/controllers/user_likes_controller.rb
+++ app/controllers/user_likes_controller.rb
@@ -1,5 +1,6 @@
 class UserLikesController < ApplicationController
-
+  skip_before_action :verify_authenticity_token
+  
   def create
     item = Item.find(params[:user_like][:id])
     UserLike.create(user: current_user, like: item)
