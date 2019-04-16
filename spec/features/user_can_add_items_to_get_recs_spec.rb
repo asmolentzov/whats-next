10cc9a24f49d305af6fcb45ce88e14a4c8eec075
Anna Smolentzov <annasmolentzov@gmail.com>
2019-04-16 10:59:23 -0600

    Updates test it block with :js

--- spec/features/user_can_add_items_to_get_recs_spec.rb
+++ spec/features/user_can_add_items_to_get_recs_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 describe 'As a user' do
-  describe 'on the profile page', js: true, vcr: true do
-    it 'can add items from Things I Like to Get Recommendations' do
+  describe 'on the profile page' do
+    it 'can add items from Things I Like to Get Recommendations', js: true do
       user = User.create(name: 'user', password: 'pass')
       hp = user.items.create(name: 'Harry Potter', item_type: 'book')
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
