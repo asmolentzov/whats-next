39b25fea33fe6b3df4d215399726e46c3f3b0764
Anna Smolentzov <annasmolentzov@gmail.com>
2019-04-15 18:16:01 -0600

    Adds and sets up VCR gem

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,15 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'webmock/rspec'
+require 'vcr'
+
+VCR.configure do |config|
+  config.configure_rspec_metadata!
+  config.cassette_library_dir = "spec/cassettes"
+  config.hook_into :webmock
+  config.filter_sensitive_data('<tastedive>') { ENV['TASTEDIVE_API_KEY'] }
+end
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
