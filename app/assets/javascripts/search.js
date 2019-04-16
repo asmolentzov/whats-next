05625f17636d8d99a96bc5c248519a3993f7fcce
Anna Smolentzov <annasmolentzov@gmail.com>
2019-04-16 15:52:19 -0600

    Adds fetch call to post to database

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
   $(".save-button").click(function(event) {
     event.preventDefault();
+    const itemId = this.parentElement.id.split('-')[1]
+    fetch('/user_likes', {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ "id": itemId })
+    })
   })
 })
