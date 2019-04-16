f00fa7ed24d1db75ea84e16757df47d45216dfc9
Anna Smolentzov <annasmolentzov@gmail.com>
2019-04-16 12:58:18 -0600

    Adds feature test for getting recommendations based on two items

--- spec/features/user_can_request_recommendations_spec.rb
+++ spec/features/user_can_request_recommendations_spec.rb
@@ -13,7 +13,7 @@ describe 'As a user' do
         expect(page).to have_css(".recommendation", count: 20)
       end
     end
-    it 'can get recommendations based on any added items', js: true, vcr: true do
+    it 'can get recommendations based on one added item', js: true, vcr: true do
       user = User.create(name: "Harry", password: "password")
       movie = user.items.create(name: 'Spirited Away', item_type: 'movie')
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
@@ -32,6 +32,31 @@ describe 'As a user' do
         expect(page).to have_css(".recommendation", count: 20)
         expect(page).to have_content('My Neighbor Totoro')
         expect(page).to have_content('Princess Mononoke')
+      end
+    end
+    it 'can get recommendations based on any added items', js: true, vcr: true do
+      user = User.create(name: "Harry", password: "password")
+      movie = user.items.create(name: 'Spirited Away', item_type: 'movie')
+      book = user.items.create(name: 'Pride and Prejudice', item_type: 'book')
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      visit profile_path
+      
+      within "#item-#{movie.id}" do
+        click_on 'Get Recommendation'
+      end
+      within "#item-#{book.id}" do
+        click_on 'Get Recommendation'
+      end
+      within '.recs-container' do
+        click_on 'Get Recommendations!'
+      end
+      
+      expect(current_path).to eq(search_path)
+      within ".reco-container" do
+        expect(page).to have_content("We Think You'll Like")
+        expect(page).to have_css(".recommendation", count: 20)
+        expect(page).to have_content('My Neighbor Totoro')
+        expect(page).to have_content('Sense And Sensibility')
       end
     end
   end
