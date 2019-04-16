4e319dd363eefed6e280c9820fea90b12505b104
Anna Smolentzov <annasmolentzov@gmail.com>
2019-04-16 15:25:49 -0600

    Adds preventDefault to button

--- spec/features/user_can_save_recommendations_spec.rb
+++ spec/features/user_can_save_recommendations_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'As a user' do
   describe 'on the search results page' do
-    it 'can save recommendations', :vcr do
+    it 'can save recommendations', js: true, vcr: true do
       user = User.create(name: "Harry", password: "password")
       movie = user.items.create(name: "Spirited Away", item_type: "movie")
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
@@ -12,7 +12,6 @@ describe 'As a user' do
       within "#recommendation-#{item.id}" do
         click_on 'Save'
       end
-      require 'pry'; binding.pry
       expect(UserLike.last.item).to eq(item)
     end
   end
