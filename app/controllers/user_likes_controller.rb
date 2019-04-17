51e7ae5cfd67699b60e04e9f7b3ecba0f29fb57c
Anna Smolentzov <annasmolentzov@gmail.com>
2019-04-16 18:34:40 -0600

    Adds route and controller for sending all of a user's saved items

--- app/controllers/user_likes_controller.rb
+++ app/controllers/user_likes_controller.rb
@@ -1,6 +1,11 @@
 class UserLikesController < ApplicationController
   skip_before_action :verify_authenticity_token
   
+  def index
+    saved_items = UserLike.where(user: current_user).map(&:like)
+    render json: saved_items
+  end
+  
   def create
     item = Item.find(params[:user_like][:id])
     UserLike.create(user: current_user, like: item)
