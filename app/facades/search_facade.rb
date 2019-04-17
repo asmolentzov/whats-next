d3d61aa16f68eb10b0f6f71aa508cfa62fef6878
Anna Smolentzov <annasmolentzov@gmail.com>
2019-04-16 21:07:44 -0600

    Updates SearchFacade to be able to return items by type

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -5,9 +5,16 @@ class SearchFacade
   end
   
   def recommendations
-    @recommendations = raw_recommendations.map do |rec|
+    @recommendations ||= raw_recommendations.map do |rec|
       Item.find_or_create_by(name: rec[:Name], item_type: rec[:Type])
     end
+  end
+  
+  def get_item_type(item_type)
+    items = recommendations.find_all do |rec|
+      rec.item_type == item_type
+    end
+    items ? items : []
   end
   
   def raw_recommendations
